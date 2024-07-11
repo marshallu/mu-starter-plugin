e2787d64d46cdb62ec1cf1765412e4555dbabbc4
Christopher McComas <mccomas.chris@gmail.com>
2024-07-11 09:35:51 -0400

    Update prompt.php

--- scripts/prompt.php
+++ scripts/prompt.php
@@ -3,7 +3,7 @@
 require 'vendor/autoload.php';
 
 use function Laravel\Prompts\text;
-use function Laravel\Prompts\progress;
+use function Laravel\Prompts\info;
 
 $plugin_name = text(
 	label: 'What is the name of your plugin?',
@@ -15,8 +15,7 @@ $plugin_description = text( 'What is the description of your plugin?' );
 $underscore_name = strtolower( str_replace( ' ', '_', $plugin_name ) );
 $hypen_name      = strtolower( str_replace( ' ', '-', $plugin_name ) );
 
-$progress = progress( label: 'Updating main plugin file', steps: 10 );
-$progress->start();
+info( 'Updating ./mu-starter-plugin.php.' );
 $path_to_file  = './mu-starter-plugin.php';
 $file_contents = file_get_contents( $path_to_file );
 $file_contents = str_replace( 'MU Starter Plugin', $plugin_name, $file_contents );
@@ -24,18 +23,16 @@ $file_contents = str_replace( "This is a starter plugin for Marshall University'
 $file_contents = str_replace( 'mu_starter_plugin', $underscore_name, $file_contents );
 $file_contents = str_replace( 'mu-starter-plugin', $hypen_name, $file_contents );
 file_put_contents( $path_to_file, $file_contents );
-$progress->finish();
 
-$progress = progress( label: 'Updating package.json', steps: 10 );
-$progress->start();
+
+info( 'Updating ./package.json.' );
 $path_to_file  = './package.json';
 $file_contents = file_get_contents( $path_to_file );
 $file_contents = str_replace( 'mu-starter-plugin', $hypen_name, $file_contents );
 file_put_contents( $path_to_file, $file_contents );
-$progress->finish();
 
-$progress = progress( label: 'Updating composer.json', steps: 10 );
-$progress->start();
+
+info( 'Updating ./composer.json.' );
 $path_to_file  = './composer.json';
 $file_contents = file_get_contents( $path_to_file );
 $file_contents = str_replace( 'MU Starter Plugin', $plugin_name, $file_contents );
@@ -43,13 +40,15 @@ $file_contents = str_replace( "This is a starter plugin for Marshall University'
 $file_contents = str_replace( 'mu_starter_plugin', $underscore_name, $file_contents );
 $file_contents = str_replace( 'mu-starter-plugin', $hypen_name, $file_contents );
 file_put_contents( $path_to_file, $file_contents );
-$progress->finish();
 
-$progress = progress( label: 'Updating README.md', steps: 10 );
-$progress->start();
+
+info( 'Updating ./README.md.' );
 $path_to_file  = './README.md';
 $file_contents = file_get_contents( $path_to_file );
 $file_contents = str_replace( 'MU Starter Plugin', $plugin_name, $file_contents );
-$file_contents = str_replace( 'Use this repository to create new WordPress plugins for use on the Marshall University WordPress multisite. This repo includes everything needed to get up and running using the Marsha WordPress theme styles.', $plugin_description, $file_contents );
+$file_contents = str_replace( 'Use this repository to create new WordPress plugins for use on the Marshall University WordPress multisite. This repo includes everything needed to get up and running using the HerdPress WordPress theme styles.', $plugin_description, $file_contents );
 file_put_contents( $path_to_file, $file_contents );
-$progress->finish();
+
+info( 'Renaming files...' );
+rename( './source/css/mu-starter-plugin.css', './source/css/' . $hypen_name . '.css' );
+rename( './mu-starter-plugin.php', './' . $hypen_name . '.php' );
